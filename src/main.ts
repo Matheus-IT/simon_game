69834c3cef31db34d2e3bd9fdf889a673dfb87b7
matheus costa <matheuscosta.tech@gmail.com>
2023-02-25 22:59:37 -0300

    Feedback with flashing and sounds

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
+import $ from 'jquery';
+
+function makeFlash(element: JQuery<HTMLElement>) {
+	element.fadeOut(100).fadeIn(100);
+}
+
+$('.btn').on('click', function (event) {
+	new Audio(`src/sounds/${event.currentTarget.id}.mp3`).play();
+	makeFlash($(event.currentTarget));
+});
+
 export {};
